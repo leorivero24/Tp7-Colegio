218258e999ecb9be926f71a4619bf9f77433ce24
leorivero24 <88672669+leorivero24@users.noreply.github.com>
2023-05-10 18:25:09 -0300

    Commit

---
+++ src/main/java/Alumno.java
@@ -0,0 +1,70 @@
+
+import java.util.HashSet;
+import javax.swing.JOptionPane;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Leonardo
+ */
+public class Alumno {
+
+    private int legajo;
+    private String apellido;
+    private String nombre;
+    private HashSet<Materia> materias = new HashSet();
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void agregarMateria(Materia materia) {
+
+      if(materias.add(materia)){
+          JOptionPane.showMessageDialog(null, "El alumno se inscribio correctamente!!");
+      }else{
+          JOptionPane.showMessageDialog(null, "Esa materia ya fue inscripta anteriormente!!");
+      }
+
+    }
+
+    public int cantidadMaterias() {
+        return materias.size();
+    }
+
+    @Override
+    public String toString() {
+        return apellido + " , " + nombre;
+    }
+
+}
---
+++ src/main/java/Materia.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+  
+    
+}
